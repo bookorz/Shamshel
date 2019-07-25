7fae92f8947a2c0e9ca6eca98871defb491c49c3
Ethan <david19870709@hotmail.com>
2019-07-25 14:53:23 +0800

    recipe 檢查只能英文檔名

--- WindowsFormsApp1/Menu/SystemSetting/FormRecipeSetting_1R1A2P.cs
+++ WindowsFormsApp1/Menu/SystemSetting/FormRecipeSetting_1R1A2P.cs
@@ -80,6 +80,13 @@ namespace Adam.Menu.SystemSetting
                 MessageBox.Show("Recipe Name or recipe id should not be empty.", "Data check error");
                 return;
             }
+            //檔名只能英文
+            System.Text.RegularExpressions.Regex reg1 = new System.Text.RegularExpressions.Regex(@"^[A-Za-z0-9]+$");
+            if (!reg1.IsMatch(tbRecipeID.Text))
+            {
+                MessageBox.Show("RecipeID must be english or numbers!");
+                return;
+            }
             //權限檢查
             using (var form = new FormConfirm("確認是否儲存 Recipe:" + tbRecipeID.Text))
             {
