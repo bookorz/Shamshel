81d712006ac8eee27daba212ba2e8c1929247ba1
steven <planet0322@gmail.com>
2019-07-30 08:24:06 +0800

    cancel login default name and password

--- WindowsFormsApp1/FormConfirm.designer.cs
+++ WindowsFormsApp1/FormConfirm.designer.cs
@@ -62,7 +62,6 @@
             this.tbPassword.PasswordChar = '*';
             this.tbPassword.Size = new System.Drawing.Size(266, 39);
             this.tbPassword.TabIndex = 69;
-            this.tbPassword.Text = "951753";
             // 
             // tbUserID
             // 
@@ -72,7 +71,6 @@
             this.tbUserID.Name = "tbUserID";
             this.tbUserID.Size = new System.Drawing.Size(266, 39);
             this.tbUserID.TabIndex = 68;
-            this.tbUserID.Text = "SANWA";
             // 
             // btnCancel
             // 
--- WindowsFormsApp1/FormLogin.designer.cs
+++ WindowsFormsApp1/FormLogin.designer.cs
@@ -60,7 +60,6 @@
             this.tbPassword.PasswordChar = '*';
             this.tbPassword.Size = new System.Drawing.Size(266, 39);
             this.tbPassword.TabIndex = 69;
-            this.tbPassword.Text = "951753";
             this.tbPassword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbUserID_KeyUp);
             // 
             // tbUserID
@@ -71,7 +70,6 @@
             this.tbUserID.Name = "tbUserID";
             this.tbUserID.Size = new System.Drawing.Size(266, 39);
             this.tbUserID.TabIndex = 68;
-            this.tbUserID.Text = "SANWA";
             this.tbUserID.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbUserID_KeyUp);
             // 
             // btnCancel
